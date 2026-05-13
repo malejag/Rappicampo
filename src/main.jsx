e617270769eee55bd0d97d0c3f6c747fd943ae33
malejag <mariaal.ortiz@udla.edu.co>
2026-05-13 14:36:14 -0500

    Corregir configuracion de Vite para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
@@ -15,5 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </CartProvider>
       </AuthProvider>
     </HashRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
